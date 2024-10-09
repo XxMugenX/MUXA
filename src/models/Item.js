306edac2fc835affd8ae241bd5e8f959bc874f9e
XxMugenX <peaceakhaze017@gmail.com>
2024-10-09 05:12:53 +0100

    models update

--- src/models/Item.js
+++ src/models/Item.js
@@ -2,24 +2,61 @@ const mongoose = require('mongoose')
 
 const schema = mongoose.Schema
 
-const ItemSchema = new schema({
-    ItemName: {type: String, required : true},
-    Category: {type: String, required : true} ,
-    Description: {type: String, required: true},
-    //user can add a main genre and sub genre
-    Genre: [{type: String, required: true}, {type: String}],
-    //stock keeping units
-    SKUs:[{
-        SKU: {type: String, required: true},
-        Price: { type: Number, required: true, 
-                currency: {type: String, maxlength:3}},
-        Quantity: {type: Boolean, required: true}
-    }],
-    Owner: {type: String, required: true}
+const CategorySchema = new schema ({
+    CategoryName : {
+        type : String
+    }
+})
+
+const SizeSchema = new schema ({
+    SizeValue : {
+        type : String
+    }
+})
+
+const ProductEntrySchema = new schema({
+    ProductId : mongoose.SchemaTypes.ObjectId,
+    CategoryId : mongoose.SchemaTypes.ObjectId,
+    SizeId : mongoose.SchemaTypes.ObjectId,
+    Sku : _id,//holds the _id of the individual entry
+    Price : {
+        type : Number, 
+        required : true
+    },
+    Quantity : {
+        type : Number, 
+        required : true
+    },
+    ProductImage : {
+        type : String
+    }
+})
+
+const ProductSchema = new schema({
+    ProductName : {
+        type : String, 
+        required : true
+    },
+    Description : {
+        type : String, 
+        required : true
+    },
+    /*Seller : mongoose.SchemaTypes.ObjectId,*/
+    CreatedAt : {
+        type : Date, 
+        immutable : true, 
+        default : () => Date.now
+    }
 }, 
-{collection: "Inventory"})
+{collection: "Products"})
 
-const Item = mongoose.model('Item', ItemSchema)
 
-module.exports = Item;
+const Product = mongoose.model('Product', ProductSchema);
+const ProductEntry = mongoose.model('ProductEntry', ProductEntrySchema);
+const Category = mongoose.model('Category', CategorySchema);
+const Size = mongoose.model('Size', SizeSchema);
+
+
+
+module.exports = [Product, ProductEntry, Category, Size];
 
--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 
 const schema = mongoose.Schema;
 
+//usertype
+
 const UserSchema = new schema({
     UserName: {type: String, required: true, unique: true},
     Password: {type: String, required: true},
