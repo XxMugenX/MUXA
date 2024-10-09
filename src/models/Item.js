1e7bfaa75cc747acef7fae5e21d41a4589f9e47b
XxMugenX <peaceakhaze017@gmail.com>
2024-10-09 13:59:11 +0100

    model and stockup page updated

--- src/models/Item.js
+++ src/models/Item.js
@@ -4,20 +4,31 @@ const schema = mongoose.Schema
 
 const CategorySchema = new schema ({
     CategoryName : {
-        type : String
+        type : String, 
+        required : true  
     }
 })
 
 const SizeSchema = new schema ({
     SizeValue : {
-        type : String
+        type : String, 
+        required : true
     }
+})
+
+const SystemRequirementSchema = new schema ({
+    SystemRequirement : {
+        type : String, 
+        required : true
+    }
+
 })
 
 const ProductEntrySchema = new schema({
     ProductId : mongoose.SchemaTypes.ObjectId,
     CategoryId : mongoose.SchemaTypes.ObjectId,
     SizeId : mongoose.SchemaTypes.ObjectId,
+    SystemRequirementId : mongoose.SchemaTypes.ObjectId,
     Sku : _id,//holds the _id of the individual entry
     Price : {
         type : Number, 
@@ -29,8 +40,18 @@ const ProductEntrySchema = new schema({
     },
     ProductImage : {
         type : String
+    },
+    ProductImageAlternative : {
+        type : String,
+        required : true
     }
-})
+},
+//correct this code, to replace the product image with the atl if the product image is not available
+//ordo it from the
+    if (ProductImage === Null) {
+        ProductImage = ProductImageAlternative
+    }
+)
 
 const ProductSchema = new schema({
     ProductName : {
@@ -55,8 +76,9 @@ const Product = mongoose.model('Product', ProductSchema);
 const ProductEntry = mongoose.model('ProductEntry', ProductEntrySchema);
 const Category = mongoose.model('Category', CategorySchema);
 const Size = mongoose.model('Size', SizeSchema);
+const SystemRequirement = mongoose.model('SystemRequirement', SystemRequirementSchema);
 
 
 
-module.exports = [Product, ProductEntry, Category, Size];
+module.exports = [Product, ProductEntry, Category, Size, SystemRequirement];
 
